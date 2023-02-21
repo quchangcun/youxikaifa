d65f3798470d8f2e8dd015be2c6e2b6341a9150b
Hollysys\quchangcun <2262274670@qq.com>
2023-02-21 14:41:08 +0800

    增加函数重用以及extern"C"说明

--- youxikaifa.cpp
+++ youxikaifa.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void fun(int a, int b)
+{
+	cout << "int fun" << endl;
+}
+void fun(double a, double b)
+{
+	cout << "double fun" << endl;
+}
+
+//不进行名字改编
+extern "C" void fun(int a)
+{
+	cout << "xxxx" << endl;
+}
+extern "C" void fun1(double a)
+{
+	cout << "yyyy" << endl;
+}
+
 int num = 100;
 
 int main()
@@ -16,14 +35,19 @@ int main()
 
 	cout << endl;
 
-	int a[30];
+	//int a[30];
 	int* p2 = new int[30];
-	p2 = a;
-	for (int i = 0; i < 29; i++)
-	{
-		a[i] = i + 3;
-		cout << a[i] << endl;
-	}
+	//p2 = a;
+	//for (int i = 0; i < 29; i++)
+	//{
+		//a[i] = i + 3;
+		//cout << a[i] << endl;
+	//}
+	delete[] p2;
 
+	fun(3, 4);
+	fun(3.3, 4.4);
+	fun(3);
+	fun1(5.5);
 	return 0;
 }
