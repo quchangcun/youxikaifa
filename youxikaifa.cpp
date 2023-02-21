42e7619599bf84e154ed2108936e918d2de5505f
Hollysys\quchangcun <2262274670@qq.com>
2023-02-21 11:25:15 +0800

    新增 New 和 Delete 指令

--- youxikaifa.cpp
+++ youxikaifa.cpp
@@ -8,5 +8,22 @@ int main()
 	int num = 30;
 	cout << num << endl;
 	cout << ::num << endl;
+	cout << endl;
+
+	int* p = new int(520);
+	cout << *p << endl;
+	delete p;
+
+	cout << endl;
+
+	int a[30];
+	int* p2 = new int[30];
+	p2 = a;
+	for (int i = 0; i < 29; i++)
+	{
+		a[i] = i + 3;
+		cout << a[i] << endl;
+	}
+
 	return 0;
 }
